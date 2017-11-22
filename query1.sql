ba5b879827abd5ffa23e2183a2047681dad2df7c
leonor-loureiro <leonor-filipa@tecnico.ulisboa.pt>
2017-11-22 21:23:58 +0000

    sql1

---
+++ query1.sql
@@ -0,0 +1,11 @@
+﻿with secundarios as (
+	select nif, categoria
+	from fornece_sec natural join produto
+), primarios as (
+	select forn_primario as nif, categoria from produto
+	except
+	select nif, categoria from fornece_sec natural join produto
+)
+
+select * from secundarios;
+
