d4e4de2b6d01627ed57230a14eb1715aeada286c
catarinacustodio <catarinacustodio@github.com>
2017-11-23 15:45:40 +0000

    NÃO USAR PARENTESIS NO GROUP BY! (funciona)

--- query1.sql
+++ query1.sql
@@ -1,11 +1,23 @@
-﻿with secundarios as (
-	select nif, categoria
-	from fornece_sec natural join produto
-), primarios as (
-	select forn_primario as nif, categoria from produto
-	except
-	select nif, categoria from fornece_sec natural join produto
+﻿WITH secundarios AS (
+	SELECT nif, categoria
+	FROM fornece_sec NATURAL JOIN produto
+), primarios AS (
+	SELECT forn_primario AS nif, categoria FROM produto
+	EXCEPT
+	SELECT nif, categoria FROM secundarios
+), total_fornecedores AS (
+	SELECT nif, categoria
+	FROM primarios
+	union
+	SELECT nif, categoria
+	FROM secundarios
 )
 
-select * from secundarios;
-
+SELECT nome
+FROM total_fornecedores NATURAL JOIN fornecedor
+GROUP BY nif, nome
+HAVING COUNT(DISTINCT categoria) >= ALL (
+	SELECT COUNT(DISTINCT categoria)
+	FROM total_fornecedores
+	GROUP BY nif, nome
+);
