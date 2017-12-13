5be5b8f33d4bf523788d701efb8ccfaf70f64c0d
leonor-loureiro <leonor.filipa@tecnico.ulisboa.pt>
2017-12-13 00:43:18 +0000

    triggers

--- schema.sql
+++ schema.sql
@@ -97,6 +97,21 @@ end
 $$ language plpgsql;
 
 
+create or replace function Supermercado.fornecedor_disjoint2() returns trigger
+as $$
+begin
+	if exists (select * from Supermercado.fornece_sec
+				where nif = new.forn_primario and ean = new.ean)
+	then
+		delete from Supermercado.fornece_sec where ean = new.ean and nif = new.forn_primario;
+		raise notice 'O fornecedor % passou de fornecedor secundario do produto % para fornecedor primario do mesmo.', new.forn_primario, new.ean;
+	end if;
+	return new;
+end
+$$ language plpgsql;
+
+
+
 /* RI-RE2: Nome nao pode existir simultaneamente em categoria_simples e em super_categoria */
 create or replace function Supermercado.categoria_simples_disjoint() returns trigger
 as $$
@@ -149,10 +164,12 @@ create table Supermercado.produto(
 	forn_primario Supermercado.NIF  not null,
 	data date  not null default CURRENT_DATE,
 	constraint pk_produto primary key (ean),
-	constraint fk_categoria foreign key (categoria) references Supermercado.categoria(nome) on delete cascade,
-	constraint fk_forn_primario foreign key (forn_primario) references Supermercado.fornecedor(nif) on delete cascade
+	constraint fk_categoria foreign key (categoria) references Supermercado.categoria(nome) on delete cascade on update cascade,
+	constraint fk_forn_primario foreign key (forn_primario) references Supermercado.fornecedor(nif) on delete cascade on update cascade
 );
-
+/* RI-EA4: O fornecedor (primario) de um produto nao pode existir na relacao fornece_sec para o mesmo produto */
+create trigger trigger_fornece_prim after update on Supermercado.produto
+	for each row execute procedure Supermercado.fornecedor_disjoint2();
 
 create table Supermercado.corredor(
 	nro Supermercado.NRO,
@@ -167,7 +184,7 @@ create table Supermercado.prateleira(
 	lado Supermercado.LADO,
 	altura Supermercado.ALTURA,
 	constraint pk_prateleira primary key(nro,lado,altura),
-	constraint fk_nro foreign key (nro) references Supermercado.corredor on delete cascade
+	constraint fk_nro foreign key (nro) references Supermercado.corredor on delete cascade on update cascade
 );
 
 
@@ -181,15 +198,15 @@ create table Supermercado.planograma(
 	unidades numeric(3,0)  not null,
 	loc numeric(3,0)  not null,
 	constraint pk_planograma primary key(ean, nro, lado, altura),
-	constraint fk_ean foreign key(ean) references Supermercado.produto on delete cascade,
-	constraint fk_nro_lado_altura foreign key(nro, lado, altura) references Supermercado.prateleira on delete cascade
+	constraint fk_ean foreign key(ean) references Supermercado.produto on delete cascade on update cascade,
+	constraint fk_nro_lado_altura foreign key(nro, lado, altura) references Supermercado.prateleira on delete cascade on update cascade
 );
 
 
 create table Supermercado.categoria_simples(
 	nome Supermercado.CAT_NOME,
 	constraint pk_categoria_simples primary key (nome),
-	constraint fk_nome foreign key (nome) references Supermercado.categoria on delete cascade
+	constraint fk_nome foreign key (nome) references Supermercado.categoria on delete cascade on update cascade
 );
 /* RI-RE2: Nome nao pode existir simultaneamente em categoria_simples e em super_categoria */
 create trigger trigger_categoria_simples before insert on Supermercado.categoria_simples
@@ -201,7 +218,7 @@ create trigger trigger_categoria_simples before insert on Supermercado.categoria
 create table Supermercado.super_categoria(
 	nome Supermercado.CAT_NOME,
 	constraint pk_super_categoria primary key (nome),
-	constraint fk_nome foreign key (nome) references Supermercado.categoria on delete cascade
+	constraint fk_nome foreign key (nome) references Supermercado.categoria on delete cascade on update cascade
 );
 /* RI-RE2: Nome nao pode existir simultaneamente em categoria_simples e em super_categoria */
 create trigger trigger_super_categoria before insert on Supermercado.super_categoria
@@ -214,8 +231,8 @@ create table Supermercado.constituida(
 	categoria Supermercado.CAT_NOME,
 	/*  RI-EA2: super_categoria != categoria_simples */
 	constraint super_diff_sub check (super_categoria != categoria),
-	constraint fk_super_categoria foreign key (super_categoria) references Supermercado.super_categoria(nome) on delete cascade,
-	constraint fk_categoria foreign key (categoria) references Supermercado.categoria(nome) on delete cascade,
+	constraint fk_super_categoria foreign key (super_categoria) references Supermercado.super_categoria(nome) on delete cascade on update cascade,
+	constraint fk_categoria foreign key (categoria) references Supermercado.categoria(nome) on delete cascade on update cascade,
 	constraint pk_constituida primary key(super_categoria, categoria)
 );
 /* RI-EA1: Nao podem existir ciclo super_categoria e categoria (1 nivel)*/
@@ -229,11 +246,11 @@ create table Supermercado.fornece_sec(
 	nif Supermercado.NIF,
 	ean Supermercado.EAN,
 	constraint pk_fornece_sec primary key (nif, ean),
-	constraint fk_ean foreign key (ean) references Supermercado.produto on delete cascade,
-	constraint fk_nif foreign key (nif) references Supermercado.fornecedor on delete cascade
+	constraint fk_ean foreign key (ean) references Supermercado.produto on delete cascade on update cascade,
+	constraint fk_nif foreign key (nif) references Supermercado.fornecedor on delete cascade on update cascade
 );
 /* RI-EA4: O fornecedor (primario) de um produto nao pode existir na relacao fornece_sec para o mesmo produto */
-create trigger trigger_fornece_sec before insert on Supermercado.fornece_sec
+create trigger trigger_fornece_sec before insert or update on Supermercado.fornece_sec
 	for each row execute procedure Supermercado.fornecedor_disjoint();
 
 
@@ -244,7 +261,7 @@ create table Supermercado.evento_reposicao(
 	constraint pk_evento_reposicao primary key (operador, instante)
 );
 /* RI-EA3​ : O instante mais recente de reposicao tem de ser sempre anterior ou igual a data atual */
-create trigger trigger_evento_reposicao before insert on Supermercado.evento_reposicao
+create trigger trigger_evento_reposicao before insert or update on Supermercado.evento_reposicao
 	for each row execute procedure Supermercado.data_anterior_atual();
 
 
@@ -258,6 +275,6 @@ create table Supermercado.reposicao(
 	instante Supermercado.INSTANTE,
 	unidades int not null,
 	constraint pk_reposicao primary key (ean, nro, lado, altura, operador, instante),
-	constraint fk_ean_nro_lado_altura foreign key (ean, nro, lado, altura) references Supermercado.planograma on delete cascade,
-	constraint fk_operador_instante foreign key (operador, instante) references Supermercado.evento_reposicao on delete cascade
+	constraint fk_ean_nro_lado_altura foreign key (ean, nro, lado, altura) references Supermercado.planograma on delete cascade on update cascade,
+	constraint fk_operador_instante foreign key (operador, instante) references Supermercado.evento_reposicao on delete cascade on update cascade
 );
